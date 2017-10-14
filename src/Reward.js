22814ab9a484afd38bb5606944944deaab168d2d
ccheung0926 <cindycheung0926@gmail.com>
2017-10-14 15:32:13 -0700

    pull

---
+++ src/Reward.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reward extends Component {
+  render() {
+    return (
+      <div className="reward">
+        <div>Reward page</div>
+      </div>
+    );
+  }
+}
+
+export default Reward;

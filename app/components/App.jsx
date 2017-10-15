cae79e27e5572de1e11a8bcf5af24e3fe4ab8fcb
ccheung0926 <cindycheung0926@gmail.com>
2017-10-14 23:31:21 -0700

    css fix

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,7 +6,11 @@ import { Link } from 'react-router-dom';
 const App = () => (
   <div>
     <header>
-      <h2>Booking Go</h2>
+      <div className="header-left">
+        <h2 className="h2">Booking Go</h2>
+        <h2 className="referral">Referral Your Friend:</h2>
+        <input value="https://youtu.be/ewzY-vXJAsU" />
+      </div>
       <div className="user">
         <p>Welcome back! Cindy</p>
         Points: 1000 points
